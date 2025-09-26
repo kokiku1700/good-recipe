edf78f1db44e568ad74fddc008919f860115c95c
kokiku1700 <kokiku1700@naver.com>
2025-09-26 23:55:20 +0900

    2025-09-26

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -4,20 +4,49 @@ const Footer = () => {
 
     return (
         <StyledFooter>
+            <H2>선한레시피</H2>
+            <Address>경기 성남시 분당구 서판교로44번길 17-3</Address>
+            <P>전화 : 031-705-3315</P>
             <StyledLink to="/street">찾아오는 길</StyledLink>
+            <Span>
+                <P>이용약관</P>
+                <P> | </P>
+                <P>개인정보처리방침</P>
+            </Span>
+            <P>"당신의 식탁에 따뜻함을 더합니다."</P>
         </StyledFooter>
     );
 };
 
 const StyledFooter = styled.footer`
     width: 100%;
-    color: white;
-    background: #1f3b2c;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 3% 0;
+    background: #e6eeeaff;
 `;
 
 const StyledLink = styled(Link)`
-    color: white;
+    
     text-decoration: none;
+`;
+
+const H2 = styled.h2`
+    margin-bottom: 3%;
+`;
+
+const Address = styled.address`
+
+`;
+
+const Span = styled.span`
+    display: flex;
+`;
+
+const P = styled.p`
+
 `;
 
 
---
+++ src/hooks/useFetch.js
@@ -0,0 +1,12 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useFetch = () => {
+    const [data, setData] = useState({});
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+
+    useEffect(() => {
+        
+    })
+}
